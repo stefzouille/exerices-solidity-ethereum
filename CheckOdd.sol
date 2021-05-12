5b5bbda546062f1c41bc5176c5028064922c0a01
stefzouille <contact.lokcy@gmail.com>
2021-05-12 10:25:02 +0200

    cor

--- CheckOdd.sol
+++ CheckOdd.sol
@@ -3,14 +3,15 @@
 pragma solidity ^0.8.0;
 
 contract CheckOdd {
-    uint256 private _check;
+    
 
     function check(uint256 value) public pure returns(bool){
         uint256 res = value % 2;
-       if(res == 0) {
+       if(res != 0) {
             return true;
        } else {
             return false;
        }
     }
 }
+// ou simplement return value % 2 == 0 ? false : true;
--- PayableCalculator.sol
+++ PayableCalculator.sol
@@ -1,64 +1,86 @@
-//SPDX-License-Identifier: MIT 
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
+
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Address.sol";
 
 contract PayableCalculator {
-    mapping(address => uint256) private _balances;
-     address private _owners;
-    uint256 public _price = 10^15;
+    
+    // library usage
     using Address for address payable;
     
-    function checkPrice(send) public {
-        require(send = _price);
+    // State variables
+    mapping(address => uint256) private _balances;
+    address private _owner;
+    uint256 private _counter;
+    
+    // Events
+    event Added(address indexed account, int256 res);
+    
+    // constructor
+    constructor() {
+        _owner = msg.sender;
+    }
+    
+    modifier hasEnoughFinney() {
+        require(msg.value >= 1e15, "PayableCalculator: not enought money, you need pay at least 1 finney to execute the function");
+        _;
+    }
+    
+    modifier isOwner(address user) {
+        require(user == _owner, "PayableCalculator: you need to be the owner to be able to withdraw");
+        _;
     }
     
     
-   function add(int256 a, int256 b) public payable returns (int256)  {
-       checkPrice(msg.value);
-        return a + b;
-    }
-        
-        
-    
-
-    function sub(int256 a, int256 b) public payable returns (int256) {
-        checkPrice(msg.value);
-        return a - b;
-    }
-
-    function mul(int256 a, int256 b) public payable returns (int256) {
-        checkPrice(msg.value);
-        return a * b;
-    }
-
-    function div(int256 a, int256 b) public payable returns (int256) {
-        checkPrice(msg.value);
-        require(b != 0, "Calc: can not devide by 0 ");
-
-        return a / b;
+    function add(int256 nb1, int256 nb2) public payable hasEnoughFinney returns(int256) {
+        _balances[_owner] += msg.value;
+        emit Added(msg.sender, nb1 + nb2);
+        _counter++;
+        return nb1 + nb2;
     }
     
-       function mod(int256 a, int256 b) public payable returns (int256) {
-           checkPrice(msg.value);
-           require(b != 0, "Calc: can not modulo by 0 ");
-        return a % b;
+    function sub(int256 nb1, int256 nb2) public payable hasEnoughFinney returns(int256) {
+        _balances[_owner] += msg.value;
+        emit Added(msg.sender, nb1 - nb2);
+        _counter++;
+        return nb1 - nb2;
     }
     
-     function _deposit(address sender, uint256 amount) private {
-        _balances[sender] += amount;
+    function mul(int256 nb1, int256 nb2) public payable hasEnoughFinney returns(int256) {
+        _balances[_owner] += msg.value;
+        emit Added(msg.sender, nb1 * nb2);
+        _counter++;
+        return nb1 * nb2;
     }
     
-    function sendValue(address payable recipient, uint256 amount) internal {
-        require(address(this).balance >= amount, "Address: insufficient balance");
-
-        // solhint-disable-next-line avoid-low-level-calls, avoid-call-value
-        (bool success, ) = recipient.call{ value: amount }("");
-        require(success, "Address: unable to send value, recipient may have reverted");
+    function div(int256 nb1, int256 nb2) public payable hasEnoughFinney returns(int256) {
+        _balances[_owner] += msg.value;
+        emit Added(msg.sender, nb1 / nb2);
+        _counter++;
+        return nb1 / nb2;
     }
     
-     function withdraw() public {
-        uint256 amount = _balances[msg.sender];
+    function mod(int256 nb1, int256 nb2) public payable hasEnoughFinney returns(int256) {
+        _balances[_owner] += msg.value;
+        emit Added(msg.sender, nb1 % nb2);
+        _counter++;
+        return nb1 % nb2;
+    }
+    
+    
+    function withdrawProfit() public isOwner(msg.sender) {
+        require(_balances[_owner] > 0, "SmartWallet: can not withdraw 0 ether");
+        uint256 amount = _balances[_owner];
+        _balances[_owner] = 0;
         payable(msg.sender).sendValue(amount);
+    }
+    
+    function seeProfit() public view returns(uint256) {
+        return _balances[_owner];
+    }
+    
+    function seeCounter() public view returns(uint256) {
+        return _counter;
     }
 }
